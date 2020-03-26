7fd97f8ecdfd740c94a75c5ab462e10cf612c54c
Samuel Berger <s@familjenberger.com>
2020-03-26 22:40:41 +0100

    Guessing game slightly done.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
-use std::io; //Use something not in the Prelude.
+use std::io;            //Use something not in the Prelude.
 use std::cmp::Ordering;
-use rand::Rng; //Use random generator imported from Cargo.
+use rand::Rng;          //Use random generator imported from Cargo.
+//use std::error::Error;  //https://stevedonovan.github.io/rust-gentle-intro/6-error-handling.html
 
 fn main() {
     println!("Guess the number");
@@ -10,33 +11,34 @@ fn main() {
     //println!("The secret number is: {}.", secret_number);
 
     loop{
-    println!("Please input your guess between 1 and 100: ");
+        println!("Please input your guess between 1 and 100: ");
 
-    //mut makes the variable mutable.
-    //new creates a new instance of String.
-    let mut guess = String::new();
-    
-    //& makes guess a mutalbe reference.
-    io::stdin().read_line(&mut guess)
+        //mut makes the variable mutable.
+        //new creates a new instance of String.
+        let mut guess = String::new();
+        
+        //& makes guess a mutalbe reference.
+        io::stdin().read_line(&mut guess)
         //readline returns a io::Result, values either Ok or Err,
         //so the method .expect can be used to handle the value.
         .expect("Failed to read line.");
-
+            
+        //This is a shadow value of the former variable guess.
         let guess: u32 = match guess.trim().parse(){
             //.expect("Please type a number."); //Now correct error handling below:
             Ok(num) => num,
+            //Continue: go to the next iteration of the loop.
             Err(_) => continue,
         };
 
-
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            println!("You win!");
-            break;
+        // Match expression made of arms. This example has three arms.
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+              println!("You win!");
+              break;
+            }
         }
     }
-}
 }
