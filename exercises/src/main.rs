30c31a81805c9dfd655da7046d074540bdd937b6
Samuel Berger <s@familjenberger.com>
2020-06-30 21:45:46 +0200

    Klar med Fibonacci.

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -1,39 +1,64 @@
+//file:///home/ghwomb/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/book/ch03-05-control-flow.html#summary
+
 fn main() {
     //Chapter 3 file:///home/sabe/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/book/ch03-05-control-flow.html
-    //f_to_c(56.0);
-    
+    f_to_c(56.0);
+    c_to_f(23.0);
+
+    fib_array(12);
+    fib_array(3);
+
+    /*
+    println!("{},", fibbonaci_worker(0));
     println!("{},", fibbonaci_worker(1));
+    println!("{},", fibbonaci_worker(2));
     println!("{},", fibbonaci_worker(3));
-    println!("{},", fibbonaci_worker(5));
-    println!("{},", fibbonaci_worker(9));
+    println!("{},", fibbonaci_worker(4));
+    */
 }
 
 //Convert from Farenheit to Celsius.
-fn f_to_c (tempc: f32) {
+fn f_to_c (tempf: f32) {
     
-    let tempf = ((tempc-32.0)*5.0)/9.0;
+    let tempc = (tempf-32.0)/1.8;
 
-    println!("{} degrees farenheit is {} degrees celsius.", tempc, tempf);
+    println!("{} degrees farenheit is {} degrees celsius.", tempf, tempc);
 }
 
+//Convert from Celsius to Farenheit.
+fn c_to_f (tempc: f32) {
+
+    let tempf = tempc * 1.8 + 32.0;
+
+    println!("{} degrees celsius is {} degrees farenheit.", tempc, tempf);
+
+}
+
+fn fib_array(end_number: u32){
+
+    let mut answers: Vec<u32> = Vec::new();
+    let mut counter = 0;
+
+    while counter != end_number {
+        answers.push( fibbonaci_worker(counter));
+        //println!("{},", fibbonaci_worker(counter));
+        counter = counter + 1;
+    }
+
+    for i in &answers {
+        println!("{},", i);
+    }
+
+}
 
 //Recursive generation of the nth Fibonacci number.
 //https://benjaminbrandt.com/fibonacci-in-rust/
 fn fibbonaci_worker(n: u32) -> u32{
     
     match n {
-        0 => 1,
+        0 => 0,
         1 => 1,
         _ => fibbonaci_worker(n-1) + fibbonaci_worker(n-2),
     }
 }
 
-
-
-/*
-let a : [i32; 5];
-
-    for element in a.iter(){
-        a[element] = 2;
-    }
-*/
